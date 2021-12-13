ea094188dba06e7bc480e8d991a1d5eaf1b4cb26
ujjwol22 <ujjwalparajulisdb6@gmail.com>
2021-12-12 18:06:49 -0800

    string datatype

---
+++ pythonassignment/Data-types/string/classnote.py
@@ -0,0 +1,21 @@
+#creating the string
+string1='Welcome to python world'
+print("string with the use of double quotes:")
+print(string1)
+ 
+# with double quotes
+string1="I'm python programmer"
+print("\n string with the use of double quotes")
+print(string1)
+
+# with triple Quotes
+string1='''I'm a python programmer and i live in the world of "python"'''
+print("\nstring with the use of triple Quotes:")
+print(string1)
+
+# Quotes allows multiple lines
+String1 = '''Python
+For
+Ml and AI'''
+print("\nCreating a multiline String: ")
+print(String1)
